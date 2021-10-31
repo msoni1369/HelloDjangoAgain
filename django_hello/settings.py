693cbe9a3c60f37b1abefc57d899a378c54c273e
Mayank Soni <41102134+msoni1369@users.noreply.github.com>
2021-10-31 22:42:30 +0530

    Update settings.py

--- django_hello/settings.py
+++ django_hello/settings.py
@@ -74,14 +74,14 @@ WSGI_APPLICATION = 'django_hello.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-"""
+
 DATABASES = {    
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }    
 }
-"""
+
 
 
 # Password validation
